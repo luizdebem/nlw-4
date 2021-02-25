a762b2e041db3b68f1f7c53dede0ebe5106e221f
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-02-25 18:02:02 -0300

    Day four | withdraw

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -13,6 +13,11 @@ defmodule RocketpayWeb.AccountsController do
     end
   end
 
-  def withdraw() do
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
   end
 end
